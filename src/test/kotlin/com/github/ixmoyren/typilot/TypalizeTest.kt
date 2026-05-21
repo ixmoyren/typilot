0c2467763ebb9ea1553272ff4921d5463169f9cb
ixmoyren <mxonren@foxmail.com>
2026-05-21 23:21:21 +0800

    Clean up unnecessary code

--- src/test/kotlin/com/github/ixmoyren/typilot/TypalizeTest.kt
+++ src/test/kotlin/com/github/ixmoyren/typilot/TypalizeTest.kt
@@ -4,7 +4,6 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class TypalizeTest : BasePlatformTestCase() {
     fun testTypstVersion() {
-        NativeLibrary.loadLibrary()
         val version = version()
         assertEquals("typst 0.14.2", version)
     }

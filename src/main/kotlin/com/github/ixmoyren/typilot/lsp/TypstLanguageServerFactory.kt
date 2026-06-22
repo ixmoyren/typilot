f8f1d312a90823b0ddc8c6d769630404185881f4
ixmoyren <mxonren@foxmail.com>
2026-06-22 19:01:46 +0800

    Tinymist can be downloaded in the control panel of LSP4IJ

--- src/main/kotlin/com/github/ixmoyren/typilot/lsp/TypstLanguageServerFactory.kt
+++ src/main/kotlin/com/github/ixmoyren/typilot/lsp/TypstLanguageServerFactory.kt
@@ -2,6 +2,7 @@ package com.github.ixmoyren.typilot.lsp
 
 import com.intellij.openapi.project.Project
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
+import com.redhat.devtools.lsp4ij.installation.ServerInstaller
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
 
 class TypstLanguageServerFactory : LanguageServerFactory {
@@ -15,4 +16,6 @@ class TypstLanguageServerFactory : LanguageServerFactory {
                 TinymistInstallerLocator(installer),
             )
         )
+
+    override fun createServerInstaller(): ServerInstaller = installer
 }
